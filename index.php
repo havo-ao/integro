04b53a4c03ecefe2d2a1459439ed4484a7cef5ed
Andrés Cañón <ao7@hotmail.es>
2020-02-19 14:16:50 -0500

    Integration of Timeline to be editable with WP

--- index.php
+++ index.php
@@ -13,6 +13,20 @@
  */
 
 get_header();
+
+
+$args_post_timeline=array(
+	'post_type'=>'integro',
+	'post_status'=>'publish',
+	'ignore_sticky_posts'=>true,
+	'post_per_page'=>-1,
+	'order'=>'ASC'
+);
+
+$timeline_data=new WP_Query($args_post_timeline);
+
+
+
 ?>
 
 	
@@ -51,32 +65,12 @@ get_header();
 
 			<div class="navbar-collapse collapse bg-dark" id="navbarsExample01" style="">
 
-				<?php
-					wp_nav_menu( array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-				) );
-				?>
-
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Link</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-						<div class="dropdown-menu" aria-labelledby="dropdown01">
-						<a class="dropdown-item" href="#">Action</a>
-						<a class="dropdown-item" href="#">Another action</a>
-						<a class="dropdown-item" href="#">Something else here</a>
-						</div>
-					</li>
-				</ul>
+							<?php
+								wp_nav_menu( array(
+									'theme_location' => 'menu-1',
+									'menu_id'        => 'primary-menu',
+							) );
+							?>
 
 			</div>
 		</nav>
@@ -329,13 +323,28 @@ get_header();
 			<p class="mb-3 text-white text-center sans-text">Un texto de prueba que demuestra quienes somos nosotros.</p>
 		</div>
 		<div class="col-lg-8 col-md-10 col-sm-12 mx-auto my-5">
+
+		<?php
+			if($timeline_data->have_posts()):
+				while($timeline_data->have_posts()):
+					$timeline_data->the_post();
+
+					$icon_timeline=get_field('icono');
+					$tit_timeline=get_field('titulo_timeline');
+					$comm_timeline=get_field('comentario_timeline');
+					$tt_timeline=get_field('titulo_tarjeta');
+					$cont_timeline=get_field('contenido_tarjeta');
+
+		?>
+
+
 			<div class="timeline text-white">
                 <div class="tm-left">
                     <div class="tm-left-icon">
-                        <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M640 256h512v-128h-512v128zm1152 640v480q0 66-47 113t-113 47h-1472q-66 0-113-47t-47-113v-480h672v160q0 26 19 45t45 19h320q26 0 45-19t19-45v-160h672zm-768 0v128h-256v-128h256zm768-480v384h-1792v-384q0-66 47-113t113-47h352v-160q0-40 28-68t68-28h576q40 0 68 28t28 68v160h352q66 0 113 47t47 113z"></path></svg>
+                        <?php echo $icon_timeline; ?>
                     </div>
-                    <span class="tm-left-t">feb 2020</span>
-                    <span class="tm-left-m">"desarrollo de prueba"</span>
+                    <span class="tm-left-t"><?php echo $tit_timeline; ?></span>
+                    <span class="tm-left-m"><?php echo $comm_timeline; ?></span>
                 </div>
                 <div class="tm-sb">
                     <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1683 1331l-166 165q-19 19-45 19t-45-19l-531-531-531 531q-19 19-45 19t-45-19l-166-165q-19-19-19-45.5t19-45.5l742-741q19-19 45-19t45 19l742 741q19 19 19 45.5t-19 45.5z"></path></svg>
@@ -344,13 +353,19 @@ get_header();
                         <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M1664 896q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"></path></svg>
                     </div>
                 </div>
-                <div class="tm-des">
-                    <span >Realización de maquetado, orden y programación de estructura</span>
+                <div class="tm-des sans-text text-white">
+                    <span ><?php echo $tt_timeline; ?></span>
                     <div class="line"></div>
-                    <p class="sans-text text-white">Duis lacinia dignissim nisl in volutpat. Etiam euismod, lacus et convallis auctor, urna mauris consectetur nisl, vel convallis nisi sem bibendum quam. Quisque justo elit, dapibus non pretium pretium, posuere quis mi. Nulla tincidunt molestie tellus. Praesent euismod imperdiet vulputate. Suspendisse interdum pulvinar fringilla. Donec vehicula nibh at rhoncus ornare. Nunc malesuada est ac ultrices pulvinar. Aliquam nec lobortis justo.</p>               
-                    <p class="sans-text text-white">Donec in ipsum rhoncus, tristique sem eu, vehicula metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus condimentum dignissim tempus. Aliquam erat volutpat. Fusce egestas placerat ultricies. Nulla id nisl venenatis, bibendum augue id, scelerisque elit. Integer at felis sit amet felis facilisis efficitur. Phasellus sodales, ligula eget finibus facilisis, quam tortor tincidunt justo, sit amet ullamcorper lacus metus eu ligula. Nunc eget accumsan diam. Nulla nec tempor lectus, eget iaculis felis.</p>
+                    <?php echo $cont_timeline; ?>
                 </div>
-            </div>
+			</div>
+			
+		<?php
+				endwhile;
+				wp_reset_postdata();
+			endif;
+		?>
+
 		</div>
 	</section>
 
@@ -418,6 +433,49 @@ get_header();
 				
 			<iframe class="container-fluid p-0 mt-3 mb-3" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.476297629023!2d-75.57535821547312!3d6.200722191037003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e46828759733bc3%3A0x914f89786874d943!2sEdificio%20Formacol!5e0!3m2!1ses-419!2sco!4v1582210474839!5m2!1ses-419!2sco" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 	
+
+<!-- Wordpress Content -->
+
+<div id="primary" class="content-area col-md-8 mx-auto">
+	<main id="main" class="site-main">
+
+	<?php
+	if ( have_posts() ) :
+
+		if ( is_home() && ! is_front_page() ) :
+			?>
+			<header>
+				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+			</header>
+			<?php
+		endif;
+
+		/* Start the Loop */
+		while ( have_posts() ) :
+			the_post();
+
+			/*
+				* Include the Post-Type-specific template for the content.
+				* If you want to override this in a child theme, then include a file
+				* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				*/
+			get_template_part( 'template-parts/content', get_post_type() );
+
+		endwhile;
+
+		//the_posts_navigation();
+
+	else :
+
+		get_template_part( 'template-parts/content', 'none' );
+
+	endif;
+	?>
+
+	</main><!-- #main -->
+</div><!-- #primary -->
+	
+
 
 
 <?php
